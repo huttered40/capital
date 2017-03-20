a401f855e335d50853867b93ed3ba7640ab0f3b0
Edward Hutter <hutter2@illinois.edu>
2017-03-20 11:12:11 -0500

    Added start features

--- ProcessorGrid/ProcessorGrid.h
+++ ProcessorGrid/ProcessorGrid.h
@@ -1,13 +1,30 @@
 #ifndef PROCESSORGRID_H_
 #define PROCESSORGRID_H_
 
+/*
+  System Includes
+*/
+#include <vector>
+
+
 class ProcessorGrid
 {
+public:
   ProcessorGrid();
   ~ProcessorGrid();
 
+  struct SubCommunicatorInfo
+  {
+    int commSize;
+    int commRank;
+    // What else?
+  }
+
   virtual MatMatMultiplication() = 0;
   virtual MatVecMultiplication() = 0;
+
+protected:
+  std::vector<SubCommunicatorInfo> SubCommunicatorTable;		// will return the index into the table, giving the information about the subcommunicator needed
 };
 
 class ProcessorGrid2D : public ProcessorGrid
