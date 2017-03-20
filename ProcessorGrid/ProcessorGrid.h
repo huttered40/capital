7eb88f840778f61a97e7465804615239674f1bb4
Edward Hutter <hutter2@illinois.edu>
2017-03-19 23:59:02 -0500

    Started long term project of programming a suite of algebraic matrix code

---
+++ ProcessorGrid/ProcessorGrid.h
@@ -0,0 +1,31 @@
+#ifndef PROCESSORGRID_H_
+#define PROCESSORGRID_H_
+
+class ProcessorGrid
+{
+  ProcessorGrid();
+  ~ProcessorGrid();
+
+  virtual MatMatMultiplication() = 0;
+  virtual MatVecMultiplication() = 0;
+};
+
+class ProcessorGrid2D : public ProcessorGrid
+{
+  ProcessorGrid2D();
+  ~ProcessorGrid2D();
+
+  virtual MatMatMultiplication();
+  virtual MatMatMultiplication();
+};
+
+class ProcessorGrid3D : public ProcessorGrid
+{
+  ProcessorGrid3D();
+  ~ProcessorGrid3D();
+
+  virtual MatMatMultiplication3D();
+  virtual MatMatMultiplication3D();
+};
+
+#endif /*PROCESSORGRID_H_*/
