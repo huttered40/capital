f76b6b9b63a26401d1552c3a9447ef3f82234fd7
Edward Hutter <hutter2@illinois.edu>
2017-03-20 11:07:14 -0500

    Cleaned up solver.h

--- parallelCholesky/solver.h
+++ parallelCholesky/solver.h
@@ -1,5 +1,5 @@
 /*
-	Defines the header class -> solver
+	Author: Edward Hutter
 */
 
 #ifndef SOLVER_H_
@@ -18,10 +18,8 @@
 
 // Need this to use fortran scalapack function
 extern void PDPOTRF(int *m, int *n, char *A, int *iA, int *jA, int *desca, int *ipiv, int *info);
-//extern void BLACS_...
-//extern void BLACS_...
 
-template <typename T>	// I want to be able to use integers, as well as floating points
+template <typename T>
 class solver
 {
 public:
@@ -43,40 +41,41 @@ private:
   void LURecurseBaseCase(int dimXstart, int dimXend, int dimYstart, int dimYend, int matrixWindow, int matrixSize, int matrixCutSize);
   void fillTranspose(int dimXstart, int dimXend, int dimYstart, int dimYend, int matrixWindow, int dir);
 
-  std::vector<std::vector<T> > matrixA;  	// Matrix contains all tracks of recursion. Builds recursively
-
-  /*
+/*
+    Each data owns a cyclic subset of the matrix (n^2/P^(2/3)) in size, which is what the 3-d p-grid algorithm allows
     Each rank must own a place to put intermediate results for L and U as well build it up
     I want to build it up in place like I did in the sequential case
+*/
 
-    I changed these matrix data structures back to 2d vectors. Remember that cyclic data distribution
-      makes this more complicated than as seen in lecture/elsewhere
-  */
+  std::vector<std::vector<T> > matrixA;  			// Matrix contains all tracks of recursion. Builds recursively
+
   std::vector<T> matrixL;
   std::vector<T> matrixLInverse;
-  std::vector<T> holdMatrix;	// this guy needs to hold the temporary matrix and can keep resizing himself
+  std::vector<T> holdMatrix;					// this guy needs to hold the temporary matrix and can keep resizing himself
   std::vector<T> holdTransposeL;
 
-  int matrixDimSize;	// nxn matrix, where n=matrixDimSize
-  int nDims;		// nDims represents the numDims of the processor grid
-  int worldRank;	// represents process rank in MPI_COMM_WORLD
-  int worldSize;  	// represents number of processors involved in computation
-  int localSize;	// represents the size length of a local 2D matrix that is held as a 1D matrix
-
-  int processorGridDimSize;		//Each data owns a cyclic subset of the matrix (n^2/P^(2/3)) in size, which is what the 3-d p-grid algorithm allows
-  int baseCaseSize;	// for quick lookUp when deciding when to stop recursing in LURecurse
+  int matrixDimSize;						// N x N matrix, where N = matrixDimSize
+  int nDims;							// Represents the numDims of the processor grid
+  int worldRank;						// Represents process rank in MPI_COMM_WORLD
+  int worldSize;  						// Represents number of processors involved in computation
+  int localSize;						// Represents the size length of a local 2D matrix that is held as a 1D matrix (can be calculated in other ways)
+  int processorGridDimSize;					// Represents the size of the 3D processor grid, its the cubic root of worldSize
+  int baseCaseSize;						// for quick lookUp when deciding when to stop recursing in LURecurse
 
   std::vector<int> gridDims;
-  std::vector<int> gridCoords;		// Do I really need these two as member variables?? Decide later...  
-  /*
-    I will want a sub-communicator for row and column of each rank for the MM
-    Maybe I need some other ones as well. Decide this later...
-  */
+  std::vector<int> gridCoords;
+
+/*
+    Sub-communicator information for 3D Grid, Row, Column, and Layer
+*/
   MPI_Comm grid3D,layerComm,rowComm,colComm,depthComm;
   int grid3DRank,layerCommRank,rowCommRank,colCommRank,depthCommRank;
   int grid3DSize,layerCommSize,rowCommSize,colCommSize,depthCommSize;
   std::map<int,int> gridSizeLookUp;
 
+/*
+    Residual information
+*/
   double matrixLNorm;
   double matrixLInverseNorm;
   double matrixANorm;
