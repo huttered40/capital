ba34e6fd5019c652d626ec3590605cb88d5bce5f
Edward Hutter <hutter2@illinois.edu>
2017-03-20 12:10:01 -0500

    Added some ideas in comments

--- ProcessorGrid/ProcessorGrid.h
+++ ProcessorGrid/ProcessorGrid.h
@@ -20,9 +20,16 @@ public:
     // What else?
   }
 
-  virtual MatMatMultiplication() = 0;
-  virtual MatVecMultiplication() = 0;
+  /*
+    Could use an argument into MatMatMultiplication that could idenify what kind of matrix (dense, square, triangular dense, sparse, data distribution, etc) that
+    could, via a switch statement call a private method that handles that specific kind of MatMatMultiplication with the correct arguments
+  */
+  virtual void MatMatMultiplication() = 0;
+  virtual void MatVecMultiplication() = 0;
 
+  virtual void DistributeDataCyclic() = 0;
+  virtual void DistributeDataBlockCyclic() = 0;
+  virtual void DistributeDataBlocked() = 0;
 protected:
   std::vector<SubCommunicatorInfo> SubCommunicatorTable;		// will return the index into the table, giving the information about the subcommunicator needed
 };
@@ -32,8 +39,12 @@ class ProcessorGrid2D : public ProcessorGrid
   ProcessorGrid2D();
   ~ProcessorGrid2D();
 
-  virtual MatMatMultiplication();
-  virtual MatVecMultiplication();
+  virtual void MatMatMultiplication();
+  virtual void MatVecMultiplication();
+  
+  virtual void DistributeDataCyclic();
+  virtual void DistributeDataBlockCyclic();
+  virtual void DistributeDataBlocked();
 };
 
 class ProcessorGrid3D : public ProcessorGrid
@@ -41,8 +52,12 @@ class ProcessorGrid3D : public ProcessorGrid
   ProcessorGrid3D();
   ~ProcessorGrid3D();
 
-  virtual MatMatMultiplication3D();
-  virtual MatVecMultiplication3D();
+  virtual void MatMatMultiplication3D();
+  virtual void MatVecMultiplication3D();
+
+  virtual void DistributeDataCyclic();
+  virtual void DistributeDataBlockCyclic();
+  virtual void DistributeDataBlocked();
 };
 
 #endif /*PROCESSORGRID_H_*/
