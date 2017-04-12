2aedd4102a2c5e2a491b441ce583175c4fef465c
Edward Hutter <hutter2@illinois.edu>
2017-04-12 01:20:40 -0500

    Added some beginning changes to matrix Multiplication code

--- Solver/matrixMult/matrixMult.h
+++ Solver/matrixMult/matrixMult.h
@@ -1,1 +1,28 @@
-.. add class code here
+/*
+	Author: Edward Hutter
+*/
+
+#ifndef MATRIX_MULT_H_
+#define MATRIX_MULT_H_
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class matrixMult
+{
+public:
+  matrixMult(void);
+  multiply(void);
+
+private:
+
+  // a ton of different kinds of matrix multiplication goes here, called from multipy method, get these from the cholesky and the one(s) I need for QR
+  // For now, these are very specific, maybe later I can generalize these to take care of all different layouts, storage, kinds of MM, rectangular MM, etc
+};
+
+#include "matrixMultImp.h"
+#include "scalapackMatrixMult.h"
+
+#endif /*MATRIX_MULT_H_*/
