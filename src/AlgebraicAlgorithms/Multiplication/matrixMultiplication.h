be645b79cadf55149861304745911429b98f5dba
Edward Hutter <hutter2@illinois.edu>
2017-08-02 23:13:47 -0500

    Added some starter code for MpiWorld and some useful comments.

---
+++ src/AlgebraicAlgorithms/Multiplication/matrixMultiplication.h
@@ -0,0 +1,20 @@
+/* Author: Edward Hutter */
+
+#ifndef MATRIXMULTIPLICATION_H_
+#define _MATRIXMULTIPLICATION_H_
+
+// Do I really need the same templates as the Matrix class?
+template<typename T, typename U>
+class matrixMultiplication
+{
+public:
+  matrixMultiplication();
+  ~matrixMultiplication();
+
+/*
+  Now how should I implement my many methods?
+  Isn't using virtual classes innefficient?
+*/ 
+};
+
+#endif /* MATRIX_MULTIPLICATION_H_ */
