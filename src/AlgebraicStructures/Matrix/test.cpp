46d6a3588a78cf2ff20257d7bef2be08df3b6073
Edward Hutter <hutter2@illinois.edu>
2017-08-06 23:04:58 -0500

    Moved some directories around to better organize.

---
+++ src/AlgebraicStructures/Matrix/test.cpp
@@ -0,0 +1,15 @@
+/* Author : Edward Hutter */
+
+// System includes
+#include <iostream>
+
+// Local includes
+#include "Matrix.h"
+
+using std::cout;
+
+int main(void)
+{
+  cout << "Ed Hutter\n";
+  return 0;
+}
