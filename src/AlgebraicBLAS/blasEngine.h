4428324003594433f323b2147a1ca6d3ea16563d
Edward Hutter <hutter2@illinois.edu>
2017-08-16 14:30:21 -0500

    Moved things around, blas code will be in a place now where many different algorithms can access it.

---
+++ src/AlgebraicBLAS/blasEngine.h
@@ -0,0 +1,40 @@
+/* Author: Edward Hutter */
+
+#ifndef BLASENGINE_H_
+#define BLASENGINE_H_
+
+
+// System includes
+
+// Local includes
+#include "./../AlgebraicStructures/Matrix/MatrixStructure.h"
+
+// We should use a host class and implement/define partial specialization classes
+
+template<typename T, typename U,
+  template<typename,typename,template<typename,typename,int> class> class MatrixStructureA,
+  template<typename,typename,template<typename,typename,int> class> class MatrixStructureB,
+  template<typename,typename,template<typename,typename,int> class> class MatrixStructureC>
+class blasEngine;
+
+template<typename T, typename U>
+class blasEngine<T,U,MatrixStructureSquare,MatrixStructureSquare,MatrixStructureSquare>
+{
+  // Lets prevent any instances of this class from being created.
+public:
+  blasEngine() = delete;
+  blasEngine(const blas3DEngine& rhs) = delete;
+  blasEngine(blasEngine&& rhs) = delete;
+  blasEngine<T,U,MatrixStructureSquare,MatrixStructureSquare,MatrixStructureSquare>& operator=(const blasEngine& rhs) = delete;
+  blasEngine<T,U,MatrixStructureSquare,MatrixStructureSquare,MatrixStructureSquare>& operator=(blasEngine&& rhs) = delete;
+  ~blasEngine() = delete;
+
+  // Engine methods
+  static void dgemm();
+
+private:
+};
+
+#include "blasEngine.hpp"
+
+#endif /* BLASENGINE_H_ */
