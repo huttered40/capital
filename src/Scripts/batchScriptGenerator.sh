60780e4df4455703c0109ea4ceb6d5cd943d90b8
Edward Hutter <hutter2@illinois.edu>
2018-03-19 01:00:40 -0500

    batch script generator for MM3D is working.

--- src/Scripts/batchScriptGenerator.sh
+++ src/Scripts/batchScriptGenerator.sh
@@ -1,36 +1,57 @@
 #!/bin/bash
 
-batchSciptName=$1
-numNodes=$2
-ppn=$3
+machineName=$1
+batchSciptName=$2
+numNodes=$3
+ppn=$4
 numPEs=$((ppn*numNodes))
-numBinaries=$4
+numBinaries=$5
 
 tag1='MM3D'
 tag2='CFR3D'
-tag3='CholeskyQR2_1D'
-tag4='CholeskyQR2_3D'
-tag5='CholeskyQR2_Tunable'
-tag6='scaLAPACK_QR'
+tag3='CQR2'
+tag4='SCALA_QR'
+tag5='SCALA_CF'
 
-cat <<-EOF > $1.sh
-#!/bin/bash
-#PBS -l nodes=$numNodes:ppn=$3:xe
-#PBS -l walltime=$5:$6:$7
-#PBS -N $1
-#PBS -e \$PBS_JOBID.err
-#PBS -o \$PBS_JOBID.out
-##PBS -m Ed
-##PBS -M hutter2@illinois.edu
-##PBS -A xyz
-#PBS -W umask=0027
+cat <<-EOF > $2.sh
+if [ "${machineName}" == "cetus" ] || [ "${machineName}" == "mira" ]
+then
+  echo "#!/bin/sh"
+elif [ "${machineName}" == "bw" ]
+then
+  echo "#!/bin/bash"
+  echo "#PBS -l nodes=$numNodes:ppn=$3:xe"
+  echo "#PBS -l walltime=$6:$7:$8"
+  echo "#PBS -N $2"
+  echo "#PBS -e \$PBS_JOBID.err"
+  echo "#PBS -o \$PBS_JOBID.out"
+  echo "##PBS -m Ed"
+  echo "##PBS -M hutter2@illinois.edu"
+  echo "##PBS -A xyz"
+  echo "#PBS -W umask=0027"
+  echo "cd \$PBS_O_WORKDIR"
+  echo "#module load craype-hugepages2M  perftools"
+  echo "#export APRUN_XFER_LIMITS=1  # to transfer shell limits to the executable"
+elif [ "${machineName}" == "theta" ]
+then
+  echo "#!/bin/sh"
+  echo "#COBALT -t $6"
+  echo "#COBALT -n ${numNodes}"
+  echo "#COBALT --attrs mcdram=cache:numa=quad"
+  echo "#COBALT -A QMCat"
+  echo "export n_nodes=\$COBALT_JOBSIZE"
+  echo "export n_mpi_ranks_per_node=${ppn}"
+  echo "export n_mpi_ranks=\$((\$n_nodes * \$n_mpi_ranks_per_node))"
+  echo "export n_openmp_threads_per_rank=4"
+  echo "export n_hyperthreads_per_core=2"
+  echo "export n_hyperthreads_skipped_between_ranks=4"
+  echo "Starting Cobalt job script"
+elif [ "${machineName}" == "stampede2" ]
+then
+  echo "dog"
+fi
 
-cd \$PBS_O_WORKDIR
-
-#module load craype-hugepages2M  perftools
-# export APRUN_XFER_LIMITS=1  # to transfer shell limits to the executable
-
-# Now I need to use a varibale for the command-line prompt, since it will change based on the binary executable,
+# Now I need to use a variable for the command-line prompt, since it will change based on the binary executable,
 #   for example, scalapack QR has multiple special inputs that take up comm-line prompts that others dont
 #   I want special functions in the inner-loop to handle this
 
@@ -38,18 +59,21 @@ cd \$PBS_O_WORKDIR
 # List all Binary Tags that this script supports
 MM3D=1
 CFR3D=2
-CholeskyQR2_1D=3
-CholeskyQR2_3D=4
-CholeskyQR2_Tunable=5
-scaLAPACK_QR=6
+CQR2=3
+SCALA_QR=4
+SCALA_CF=5
 
 # Each Binary tag has a specific number of command-line arguments that it accepts
-let numArgs${tag1}=19
-let numArgs${tag2}=12
-let numArgs${tag3}=15
-let numArgs${tag4}=15
-let numArgs${tag5}=23
-let numArgs${tag6}=23
+let numArgs${tag1}_SS=8
+let numArgs${tag2}_SS=1000
+let numArgs${tag3}_SS=1000
+let numArgs${tag4}_SS=1000
+let numArgs${tag5}_SS=1000
+let numArgs${tag1}_WS=14
+let numArgs${tag2}_WS=1000
+let numArgs${tag3}_WS=1000
+let numArgs${tag4}_WS=1000
+let numArgs${tag5}_WS=1000
 
 
 updateCounter () {
@@ -70,16 +94,40 @@ updateCounter () {
   echo "\$counter"
 }
 
+# Functions that write the actual script, depending on machine
+launchJobs () {
+  local numProcesses=\$((\$1 * $ppn))
+  if [ "$machineName" == "mira" ] || [ "$machineName" == "cetus" ]
+  then
+    echo "runjob --np \$numProcesses -p $ppn --block \$COBALT_PARTNAME --verbose=INFO : \${@:2:9}"
+  elif [ "$machineName" == "bw" ]
+  then
+    echo "aprun -n \$numProcesses \$@"
+  elif [ "$machineName" == "theta" ]
+  then
+    echo "#aprun -n $numNodes -N \$n_mpi_ranks_per_node --env OMP_NUM_THREADS=\$n_openmp_threads_per_rank -cc depth -d \$n_hyperthreads_skipped_between_ranks -j \$n_hyperthreads_per_core "$@""
+  elif [ "$machineName" == "stampede2" ]
+  then
+    echo "dog"
+  fi
+}
+
 # Functions for launching specific jobs based on certain parameters
 launch$tag1 () {
   # launch MM3D
-  local startDimensionK=\$6
-  local endDimensionK=\$7
-  while [ \$startDimensionK -le \$endDimensionK ];
-  do
-    echo "aprun -n \$3 \$1 \$4 \$5 \$startDimensionK \$2"
-    startDimensionK=\$(updateCounter \$startDimensionK \$9 \$8)
-  done
+  if [ \$1 == 'SS' ]
+  then
+    local startNumNodes=\$4
+    local endNumNodes=\$5
+    while [ \$startNumNodes -le \$endNumNodes ];
+    do
+        launchJobs \$startNumNodes \$2 0 1 \${11} \$8 \$9 \${10} \$3 
+        startNumNodes=\$(updateCounter \$startNumNodes \$7 \$6)
+    done
+  elif [ \$2 == 'WS' ]
+  then
+    echo "dog"
+  fi
 }
 
 launch$tag2 () {
@@ -140,30 +188,36 @@ launch$tag6 () {
   done
 }
 
-numArguments$tag1 () {
-  echo "\$numArgs$tag1"
+numArguments${tag1}_SS () {
+  echo "\$numArgs${tag1}_SS"
 }
-
-numArguments$tag2 () {
-  echo "\$numArgs$tag2"
+numArguments${tag1}_WS () {
+  echo "\$numArgs${tag1}_WS"
 }
-
-numArguments$tag3 () {
-  echo "\$numArgs$tag3"
+numArguments${tag2}_SS () {
+  echo "\$numArgs${tag2}_SS"
 }
-
-numArguments$tag4 () {
-  echo "\$numArgs$tag4"
+numArguments${tag2}_WS () {
+  echo "\$numArgs${tag2}_WS"
 }
-
-numArguments$tag5 () {
-  echo "\$numArgs$tag5"
+numArguments${tag3}_SS () {
+  echo "\$numArgs${tag3}_SS"
 }
-
-numArguments$tag6 () {
-  echo "\$numArgs$tag6"
+numArguments${tag3}_WS () {
+  echo "\$numArgs${tag3}_WS"
 }
-
+numArguments${tag4}_SS () {
+  echo "\$numArgs${tag4}_SS"
+}
+numArguments${tag4}_WS () {
+  echo "\$numArgs${tag4}_WS"
+}
+numArguments${tag5}_SS () {
+  echo "\$numArgs${tag5}_SS"
+}
+numArguments${tag5}_WS () {
+  echo "\$numArgs${tag5}_WS"
+}
 
 commandLineCounter=1
 index=1
@@ -175,63 +229,70 @@ do
   index=\$((\$commandLineCounter+1))
   export binaryTag=\${!index}
   index=\$((\$commandLineCounter+2))
-  export numIterations=\${!index}
+  export scale=\${!index}
   index=\$((\$commandLineCounter+3))
-  export startNumPEs=\${!index}
+  export numIterations=\${!index}
   index=\$((\$commandLineCounter+4))
-  export endNumPEs=\${!index}
+  export startNumNodes=\${!index}
   index=\$((\$commandLineCounter+5))
-  export jumpNumPEs=\${!index}
+  export endNumNodes=\${!index}
   index=\$((\$commandLineCounter+6))
-  export jumpNumPEsOperator=\${!index}
+  export jumpNumNodes=\${!index}
   index=\$((\$commandLineCounter+7))
+  export jumpNumNodesoperator=\${!index}
+  index=\$((\$commandLineCounter+8))
 
-  while [ \$startNumPEs -le \$endNumPEs ];
-  do
-    index=\$((\$commandLineCounter+7))
-    export startDimensionM=\${!index}
-    index=\$((\$commandLineCounter+8))
-    export endDimensionM=\${!index}
-    index=\$((\$commandLineCounter+9))
-    export jumpDimensionM=\${!index}
-    index=\$((\$commandLineCounter+10))
-    export jumpDimensionMOperator=\${!index}
-    index=\$((\$commandLineCounter+11))
-
-    while [ \$startDimensionM -le \$endDimensionM ];
-    do
-      if [ \${!binaryTag} -eq \$CFR3D ]
-      then
-        # call function
-        index=\$((\$commandLineCounter+11))
-        export blockSizeMultiplierRangeStart=\${!index}
-        index=\$((\$commandLineCounter+12))
-        export blockSizeMultiplierRangeEnd=\${!index}
-        launch\$binaryTag \$binaryPath \$numIterations \$startNumPEs \$startDimensionM \$blockSizeMultiplierRangeStart \$blockSizeMultiplierRangeEnd
-      else
-        index=\$((\$commandLineCounter+11))
-        export startDimensionN=\${!index}
-        index=\$((\$commandLineCounter+12))
-        export endDimensionN=\${!index}
-        index=\$((\$commandLineCounter+13))
-        export jumpDimensionN=\${!index}
-        index=\$((\$commandLineCounter+14))
-        export jumpDimensionNOperator=\${!index}
-        index=\$((\$commandLineCounter+15))
-        while [ \$startDimensionN -le \$endDimensionN ];
-        do
-          # call function
-          export startRange=\$((\$commandLineCounter+15))
-          export lengthTag=\$(numArguments\$binaryTag)
-          export lengthRange=\$((\$lengthTag - 15))
-          launch\$binaryTag \$binaryPath \$numIterations \$startNumPEs \$startDimensionM \$startDimensionN \${@:\$startRange:\$lengthRange}
-          startDimensionN=\$(updateCounter \$startDimensionN \$jumpDimensionNOperator \$jumpDimensionN)
-        done
-      fi
-      startDimensionM=\$(updateCounter \$startDimensionM \$jumpDimensionMOperator \$jumpDimensionM)
-    done
-    startNumPEs=\$(updateCounter \$startNumPEs \$jumpNumPEsOperator \$jumpNumPEs)
-  done
-  commandLineCounter=\$((\$commandLineCounter+numArguments\$binaryTag))
+  if [ \$binaryTag == 'MM3D' ]
+  then
+    if [ \$scale == 'SS' ]
+    then
+      export DimensionM=\${!index}
+      index=\$((\$commandLineCounter+9))
+      export DimensionN=\${!index}
+      index=\$((\$commandLineCounter+10))
+      export DimensionK=\${!index}
+      index=\$((\$commandLineCounter+11))
+      export bcastVSallgath=\${!index}
+      index=\$((\$commandLineCounter+12))
+      launch\$binaryTag \$scale \$binaryPath \$numIterations \$startNumNodes \$endNumNodes \$jumpNumNodes \$jumpNumNodesoperator \$DimensionM \$DimensionN \$DimensionK \$bcastVSallgath
+      commandLineCounter=\$((\$commandLineCounter+numArguments\${binaryTag}_\${scale}))
+    elif [ \$scale == 'WS' ]
+    then
+      export startDimensionM=\${!index}
+      index=\$((\$commandLineCounter+9))
+      export jumpDimensionM=\${!index}
+      index=\$((\$commandLineCounter+10))
+      export jumpDimensionMoperator=\${!index}
+      index=\$((\$commandLineCounter+11))
+      export startDimensionN=\${!index}
+      index=\$((\$commandLineCounter+12))
+      export jumpDimensionN=\${!index}
+      index=\$((\$commandLineCounter+13))
+      export jumpDimensionNoperator=\${!index}
+      index=\$((\$commandLineCounter+14))
+      export startDimensionK=\${!index}
+      index=\$((\$commandLineCounter+15))
+      export jumpDimensionK=\${!index}
+      index=\$((\$commandLineCounter+16))
+      export jumpDimensionKoperator=\${!index}
+      index=\$((\$commandLineCounter+17))
+      export bcastVSallgath=\${!index}
+      index=\$((\$commandLineCounter+18))
+      launch\$binaryTag \$scale \$binaryPath \$numIterations \$startNumNodes \$endNumNodes \$jumpNumNodes \$jumpNumNodesoperator \$startDimensionM \$jumpDimensionM \$jumpDimensionMoperator \$startDimensionN \$jumpDimensionN \$jumpDimensionNoperator \$startDimensionK \$jumpDimensionK \$jumpDimensionKoperator \$bcastVSallgath
+      commandLineCounter=\$((\$commandLineCounter+numArguments\${binaryTag}_\${scale}))
+    fi
+  elif [ \$binaryTag == 'CFR3D' ]
+  then
+    echo "dog"
+  elif [ \$binaryTag == 'CQR2' ]
+  then
+    echo "dog"
+  elif [ \$binaryTag == 'SCALA_QR' ]
+  then
+    echo "dog"
+  elif [ \$binaryTag == 'SCALA_CF' ]
+  then
+    echo "dog"
+  fi
 done
 EOF
--- src/Scripts/batchScriptGeneratorPrompt.sh
+++ src/Scripts/batchScriptGeneratorPrompt.sh
@@ -8,7 +8,7 @@ echo "                                4 -> /"
 
 echo $'\n'
 echo "When generating the script via batchScriptGenerator.sh, the following must be specified as command-line arguments:"
-echo "[script name]  [number of nodes]  [processor per node]  [number of binaries to test on] [number of hours for run]"
+echo "[machine name (cetus,mira,theta,stampede2,bw)] [script name]  [number of nodes]  [processes per node]  [number of binaries to test on] [number of hours for run]"
 echo "[number of minutes for run]  [number of seconds for run]"
 
 echo $'\n'
@@ -16,14 +16,24 @@ echo $'\n'
 echo "Once script has been generated, the following must be specied as command-line arguments:"
 echo "For each of the [number of binaries to test on], you must specify:"
 echo "  [binary path and name]"
-echo "  Binary Tag (MM3D, CFR3D, CholeskyQR2_1D, CholeskyQR2_3D, Cholesky_QR2_Tunable, scaLAPACK_QR)"
+echo "  Binary Tag [MM3D,CFR3D,CQR2,SCALA_QR,SCALA_CF]"
+echo " [SS,WS]"
 echo "  [number of iterations for each specific run]"
-echo "  [number of processors to start] [number of processors to end] [jump amount] [jump operator (+,-,*,/]"
-echo "  [dimension m to start] [dimension m to end] [jump amount] [jump operator (+,-,*,/]"
-echo "  if not CFR3D, specify: [dimension n to start] [dimension n to end] [jump amount] [jump operator (+,-,*,/]"
+echo "  [number of processors to start] [number of processors to end] [jump amount] [jump operator (+,-,*,/)]"
+echo "  [dimension m to start] [dimension m to end] [jump amount] [jump operator (+,-,*,/)]"
+echo "  if not CFR3D, specify: [dimension n to start] [dimension n to end] [jump amount] [jump operator (+,-,*,/)]"
+echo $'\n'
 echo "    Based on Binary Tag, more options are available."
-echo "    If Binary Tag == MM3D, specify: [dimension k to start] [dimension k to end] [jump amount] [jump operator (+,-,*,/]"
-echo "    If Binary Tag == CFR3D, specify: [start of range of the number of powers of 2 greater than the theoretically optimal base case] [end of that range]"
+echo "    If Binary Tag == MM3D"
+echo "      specify:"
+echo "          [dimension k to start] [dimension k to end] [jump amount] [jump operator (+,-,*,/)]"
+echo "          No option to try TRMM-based MM3D, just the normal GEMM"
+echo "          Only performance testing option, not sequential validation, is allowed."
+echo "          Choice of Broadcast [0] or Allgather [1] based routine"
+echo $'\n'
+echo "    If Binary Tag == CFR3D",
+echo "      specify:"
+echo "          [start of range of the number of powers of 2 greater than the theoretically optimal base case] [end of that range]"
 echo "    If Binary Tag == CholeskyQR2_1D, specify: nothing is needed"
 echo "    If Binary Tag == CholeskyQR2_3D, specify: nothing is needed"
 echo "    If Binary Tag == CholeskyQR2_Tunable, specify: [processor grid dimension d to start] [processor grid dimension d to end] [jump amount] [jump operator (+,-,*,/]"
