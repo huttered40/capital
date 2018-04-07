fcb818053a960befdc688d7a78c512ebb5c8bb58
Edward <hutter2@illinois.edu>
2018-04-07 17:56:58 -0500

    New bug fixes after testing on Porter.

--- src/Scripts/plotScript.sh
+++ src/Scripts/plotScript.sh
@@ -13,22 +13,29 @@ then
   export SCRATCH=/projects/QMCat/huttered
 elif [ "$(hostname |grep "stampede2")" != "" ]
 then
+  echo "dog"
 fi
 
 read -p "Enter the directory name within ${SCRATCH} where the results are hidden: " resultsDir
 cd ../../../../myData/
 
-if [ "$(hostname |grep "porter")" != "" ]
+read -p "Enter the machine from which you want to grab data: " machineName
+
+if [ "${machineName}" == "PORTER" ]
 then
-  scp -r hutter2@porter.cs.illinois.edu:~/hutter2/PAA_data/${resultsDir} .
-elif [ "$(hostname |grep "mira")" != "" ] || [ "$(hostname |grep "cetus")" != "" ]
+  scp -r hutter2@porter.cs.illinois.edu:~/hutter2/PAA_data/${resultsDir}.tar .
+elif [ "${machineName}" == "MIRA" ]
 then
-  scp -r huttered@miradtn.alcf.anl.gov:~/hutter2/PAA_data/${resultsDir} .
-elif [ "$(hostname |grep "theta")" != "" ]
+  scp -r huttered@miradtn.alcf.anl.gov:~/hutter2/PAA_data/${resultsDir}.tar .
+elif [ "${machineName}" == "CETUS" ]
 then
-  scp -r huttered@thetadtn.alcf.anl.gov:~/hutter2/PAA_data/${resultsDir} .
-elif [ "$(hostname |grep "stampede2")" != "" ]
+  scp -r huttered@cetusdtn.alcf.anl.gov:~/hutter2/PAA_data/${resultsDir}.tar .
+elif [ "${machineName}" == "THETA" ]
 then
+  scp -r huttered@thetadtn.alcf.anl.gov:~/hutter2/PAA_data/${resultsDir}.tar .
+elif [ "${machineName}" == "STAMPEDE2" ]
+then
+  echo "dog"
 fi
 
 tar -xvf ${resultsDir}.tar
@@ -39,4 +46,4 @@ scriptDir=$(pwd)
 
 # Generate the Makefile for Scaplot
 cd ${scaplotDir}
-bash ${scriptDir}/../Results/${resultsDir}/plotInstructions.sh | bash MakePlotScript.sh
+bash ../../myData/${resultsDir}/plotInstructions.sh | bash MakePlotScript.sh
