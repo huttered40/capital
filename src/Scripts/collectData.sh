a436211320c7ab1de3d1fe51b27a41b4edba8356
Edward Hutter <hutter2@illinois.edu>
2018-04-21 16:37:22 -0500

    Collecting data for Porter trial run+plot, before I make critter plotting changes to scripts.

--- src/Scripts/collectData.sh
+++ src/Scripts/collectData.sh
@@ -1,1 +1,1 @@
-bash ../../../PAA_data/benchQR1_2018-04-21-16_25_12_PORTER/collectInstructions.sh | bash packageData.sh
+bash ../../../PAA_data/benchQR1_2018-04-21-16_29_43_PORTER/collectInstructions.sh | bash packageData.sh
