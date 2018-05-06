369cb323685d0f936604e48b9a25e67502e8a979
Edward Hutter <tg849075@login4.stampede2.tacc.utexas.edu>
2018-05-05 21:42:30 -0500

    Running first strong scaling CFR3D test.

--- src/Scripts/collectData.sh
+++ src/Scripts/collectData.sh
@@ -1,1 +1,1 @@
-bash /scratch/05608/tg849075/benchQR1_2018-05-05-12_03_08_STAMPEDE2_1_2/collectInstructions.sh | bash packageData.sh
+bash /scratch/05608/tg849075/benchCF1_2018-05-05-21_32_09_STAMPEDE2_1_2/collectInstructions.sh | bash packageData.sh
