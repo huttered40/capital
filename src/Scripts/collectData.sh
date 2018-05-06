eb29672da8d42e5726807e3f22392285dbefb454
Edward Hutter <tg849075@login4.stampede2.tacc.utexas.edu>
2018-05-05 22:21:02 -0500

    New strong scaling analysis tests.

--- src/Scripts/collectData.sh
+++ src/Scripts/collectData.sh
@@ -1,1 +1,1 @@
-bash /scratch/05608/tg849075/benchCF1_2018-05-05-21_50_30_STAMPEDE2_1_2/collectInstructions.sh | bash packageData.sh
+bash /scratch/05608/tg849075/benchCF1_2018-05-05-22_14_21_STAMPEDE2_1_1/collectInstructions.sh | bash packageData.sh
