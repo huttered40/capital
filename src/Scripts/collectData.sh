300bcbf3d867e9778599001971e4592b8313b719
Edward Francis Hutter <hutter@h2ologin2.ncsa.illinois.edu>
2018-09-21 11:51:34 -0500

    Adding new critter/timing results for a strong scaling run.

--- src/Scripts/collectData.sh
+++ src/Scripts/collectData.sh
@@ -1,1 +1,1 @@
-bash /scratch/sciteam/hutter/benchQR2_2018-09-17-21_00_56_BLUEWATERS/collectInstructions.sh | bash packageData.sh
+bash /scratch/sciteam/hutter/benchQR09211_2018-09-21-08_37_17_BLUEWATERS/collectInstructions.sh | bash packageData.sh
