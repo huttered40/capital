2a2970b624f75f0bb0bf089b7187b770f48c7c02
Edward Hutter <hutter2@illinois.edu>
2018-09-22 23:29:01 -0500

    Added new strong scaling test on Stampede2

--- src/Scripts/collectData.sh
+++ src/Scripts/collectData.sh
@@ -1,1 +1,1 @@
-bash /scratch/sciteam/hutter/benchQR09220_2018-09-22-18_18_08_BLUEWATERS/collectInstructions.sh | bash packageData.sh
+bash /scratch/05608/tg849075/benchQR09220_2018-09-22-21_35_46_STAMPEDE2_1_1/collectInstructions.sh | bash packageData.sh
