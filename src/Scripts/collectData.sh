703a2287e409d8a81bae4bd17fa6ff08820b4dab
Edward Francis Hutter <hutter@h2ologin2.ncsa.illinois.edu>
2018-09-22 20:13:05 -0500

    Adding new strong scaling run with larger matrix sizes.

--- src/Scripts/collectData.sh
+++ src/Scripts/collectData.sh
@@ -1,1 +1,1 @@
-bash /scratch/sciteam/hutter/benchQR09211_2018-09-21-08_37_17_BLUEWATERS/collectInstructions.sh | bash packageData.sh
+bash /scratch/sciteam/hutter/benchQR09220_2018-09-22-18_18_08_BLUEWATERS/collectInstructions.sh | bash packageData.sh
