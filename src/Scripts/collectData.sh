b6b2aa054a87af6e1da4fab000f2d4e7e4ca692c
Edward Hutter <hutter2@illinois.edu>
2018-10-10 20:51:53 -0500

    Added new weak scaling test run to see if plots look good.

--- src/Scripts/collectData.sh
+++ src/Scripts/collectData.sh
@@ -1,1 +1,1 @@
-bash /scratch/05608/tg849075/benchQRIPDPS19FinalCritterSS_2018-10-09-19_53_49_STAMPEDE2_1_2/collectInstructions.sh | bash packageData.sh
+bash /scratch/05608/tg849075/benchQRTestNewWeakScalingCode_2018-10-10-20_38_08_STAMPEDE2_1_1/collectInstructions.sh | bash packageData.sh
