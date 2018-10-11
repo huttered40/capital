83fb7c13ae8a8a4ac7754df29de5b28614e1503c
Edward Hutter <hutter2@illinois.edu>
2018-10-11 17:47:09 -0500

    Adding results files from IPDPS19 Strong Scaling Critter run on Stampede2

--- src/Scripts/collectData.sh
+++ src/Scripts/collectData.sh
@@ -1,1 +1,1 @@
-bash /scratch/05608/tg849075/benchQRIPDPS19FinalCritterWS_2018-10-11-02_06_39_STAMPEDE2_1_2/collectInstructions.sh | bash packageData.sh
+bash /scratch/05608/tg849075/benchQRIPDPS19FinalCritterSS_2018-10-09-19_53_49_STAMPEDE2_1_2/collectInstructions.sh | bash packageData.sh
