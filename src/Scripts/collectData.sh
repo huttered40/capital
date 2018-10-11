f0852d093bc0286edc88e514a059deaa4c5bad6f
Edward Hutter <hutter2@illinois.edu>
2018-10-11 00:53:32 -0500

    Adding another set of weak scaling data to help debug plotting code.

--- src/Scripts/collectData.sh
+++ src/Scripts/collectData.sh
@@ -1,1 +1,1 @@
-bash /scratch/05608/tg849075/benchQRTestNewWeakScalingCode_2018-10-10-20_38_08_STAMPEDE2_1_1/collectInstructions.sh | bash packageData.sh
+bash /scratch/05608/tg849075/benchQRTestWeakScalingNew_2018-10-11-00_47_12_STAMPEDE2_1_1/collectInstructions.sh | bash packageData.sh
