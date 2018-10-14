6111c49110f67b39771232a0ca765f7018e66306
Edward Francis Hutter <hutter@h2ologin2.ncsa.illinois.edu>
2018-10-14 10:28:30 -0500

    Adding IPDPS19 Weak Scaling performance comparison on Blue Waters.

--- src/Scripts/collectData.sh
+++ src/Scripts/collectData.sh
@@ -1,1 +1,1 @@
-bash /scratch/sciteam/hutter/benchQRIPDPS19StrongScalingPerformanceBlueWaters_2018-10-13-05_06_53_BLUEWATERS/collectInstructions.sh | bash packageData.sh
+bash /scratch/sciteam/hutter/benchQRIPDPS19WeakScalingPerformanceBlueWaters_2018-10-13-18_04_35_BLUEWATERS/collectInstructions.sh | bash packageData.sh
