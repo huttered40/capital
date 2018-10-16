6e8c98d118821a794550e9ece4c30d5ae65118ea
Edward Hutter <hutter2@illinois.edu>
2018-10-16 02:31:19 -0500

    Adding final emergency data for Strong Scaling Stampede2

--- src/Scripts/collectData.sh
+++ src/Scripts/collectData.sh
@@ -1,1 +1,1 @@
-bash /scratch/05608/tg849075/benchQRIPDPS19WeakScalingPerformanceStampede2Fix3_2018-10-15-12_42_28_STAMPEDE2_2_2/collectInstructions.sh | bash packageData.sh
+bash /scratch/05608/tg849075/benchQRIPDPS19StrongScalingPerformanceStampede2Fix1_2018-10-15-12_23_31_STAMPEDE2_2_2/collectInstructions.sh | bash packageData.sh
