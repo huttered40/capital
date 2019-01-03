d147a91653934962dc74855b561772708496f274
Edward Hutter <hutter2@illinois.edu>
2019-01-03 00:10:22 -0600

    Adding new Weak scaling test with full scalapack data up to 1024 nodes

--- src/Scripts/collectDataStage1.sh
+++ src/Scripts/collectDataStage1.sh
@@ -1,1 +1,1 @@
-bash /scratch/05608/tg849075/benchQR_launch3_2019-01-02-03_07_13_STAMPEDE2_round1/collectInstructionsStage1.sh | bash packageDataStage1.sh
+bash /scratch/05608/tg849075/benchQR_launch5_2019-01-02-16_55_11_STAMPEDE2_round1/collectInstructionsStage1.sh | bash packageDataStage1.sh
--- src/Scripts/collectDataStage2.sh
+++ src/Scripts/collectDataStage2.sh
@@ -1,1 +1,1 @@
-bash ../../../PAA_data/benchQR_launch3_STAMPEDE2/collectInstructionsStage2.sh | bash packageDataStage2.sh
+bash ../../../PAA_data/benchQR_launch5_STAMPEDE2/collectInstructionsStage2.sh | bash packageDataStage2.sh
