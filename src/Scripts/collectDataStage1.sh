8ad6658ab6c6108d8977ebcc4ad198b0b14e9cc0
Edward Hutter <hutter2@illinois.edu>
2019-01-04 03:54:15 -0600

    Adding first big strong scaling test, 2097152x4096, up to 1024 nodes.

--- src/Scripts/collectDataStage1.sh
+++ src/Scripts/collectDataStage1.sh
@@ -1,1 +1,1 @@
-bash /scratch/05608/tg849075/benchQR_launch8_2019-01-03-15_56_21_STAMPEDE2_round1/collectInstructionsStage1.sh | bash packageDataStage1.sh
+bash /scratch/05608/tg849075/benchQR_launch10_2019-01-03-21_53_45_STAMPEDE2_round1/collectInstructionsStage1.sh | bash packageDataStage1.sh
