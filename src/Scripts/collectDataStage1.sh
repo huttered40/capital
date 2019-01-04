18e1e7351bc9ff3e485c266e2c292be7a8f4eb34
Edward Hutter <hutter2@illinois.edu>
2019-01-03 20:27:57 -0600

    New large weak scaling run results.

--- src/Scripts/collectDataStage1.sh
+++ src/Scripts/collectDataStage1.sh
@@ -1,1 +1,1 @@
-bash /scratch/05608/tg849075/benchQR_launch5_2019-01-02-16_55_11_STAMPEDE2_round1/collectInstructionsStage1.sh | bash packageDataStage1.sh
+bash /scratch/05608/tg849075/benchQR_launch8_2019-01-03-15_56_21_STAMPEDE2_round1/collectInstructionsStage1.sh | bash packageDataStage1.sh
