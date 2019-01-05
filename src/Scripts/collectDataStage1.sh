bc2af232730007b3eefd306348f6c141d3ac18f9
Edward Hutter <hutter2@illinois.edu>
2019-01-05 14:21:42 -0600

    Adding 3rd and final weak scaling test needed for the paper

--- src/Scripts/collectDataStage1.sh
+++ src/Scripts/collectDataStage1.sh
@@ -1,1 +1,1 @@
-bash /scratch/05608/tg849075/benchQR_launch15_2019-01-04-15_13_34_STAMPEDE2_round1/collectInstructionsStage1.sh | bash packageDataStage1.sh
+bash /scratch/05608/tg849075/benchQR_launch20_2019-01-04-23_04_49_STAMPEDE2_round1/collectInstructionsStage1.sh | bash packageDataStage1.sh
--- src/Scripts/collectDataStage2.sh
+++ src/Scripts/collectDataStage2.sh
@@ -1,1 +1,1 @@
-bash ../../../PAA_data/benchQR_launch15_STAMPEDE2/collectInstructionsStage2.sh | bash packageDataStage2.sh
+bash ../../../PAA_data/benchQR_launch20_STAMPEDE2/collectInstructionsStage2.sh | bash packageDataStage2.sh
