62bc303a26f6f3aaa4dcf19001b92b129050e56a
Edward Hutter <hutter2@illinois.edu>
2019-01-04 21:15:48 -0600

    Committing 3rd strong scaling run, 524288x8192

--- src/Scripts/collectDataStage1.sh
+++ src/Scripts/collectDataStage1.sh
@@ -1,1 +1,1 @@
-bash /scratch/05608/tg849075/benchQR_launch12_2019-01-04-04_14_57_STAMPEDE2_round1/collectInstructionsStage1.sh | bash packageDataStage1.sh
+bash /scratch/05608/tg849075/benchQR_launch15_2019-01-04-15_13_34_STAMPEDE2_round1/collectInstructionsStage1.sh | bash packageDataStage1.sh
--- src/Scripts/collectDataStage2.sh
+++ src/Scripts/collectDataStage2.sh
@@ -1,1 +1,1 @@
-bash ../../../PAA_data/benchQR_launch12_STAMPEDE2/collectInstructionsStage2.sh | bash packageDataStage2.sh
+bash ../../../PAA_data/benchQR_launch15_STAMPEDE2/collectInstructionsStage2.sh | bash packageDataStage2.sh
