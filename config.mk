149ee62430c50ba44199a4b157f81c6be9349acd
Edward Hutter <hutter2@illinois.edu>
2021-03-19 18:12:22 -0500

    more cleanup to config file

--- config.mk
+++ config.mk
@@ -2,12 +2,15 @@ BIN=$(HOME)/capital/bin/
 
 ## For Stampede2
 #critter_dir=$(HOME)/critter
-CCMPI=mpicxx
+#CCMPI=mpicxx
+CCMPI=
 #INCLUDES=-I$(critter_dir)/include
 INCLUDES=
 #DEFS=-DMKL -DCRITTER -DALGORITHMIC_SYMBOLS
 DEFS=
-CFLAGS=-g -Wall -O3 -std=c++14 -mkl=parallel -xMIC-AVX512 ${DEFS} ${INCLUDES}
+#CFLAGS=-g -Wall -O3 -std=c++14 -mkl=parallel -xMIC-AVX512 ${DEFS} ${INCLUDES}
+CFLAGS=${DEFS} ${INCLUDES}
 #LIB_PATH=-L$(critter_dir)/lib
 LIB_PATH=
+#LIBS=-lcritter
 LIBS=
