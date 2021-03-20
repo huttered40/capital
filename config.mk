fb53eee8021c3d636dc0fdd99a6768d0e0bf4b87
Tal Ben-Nun <talbn@inf.ethz.ch>
2021-03-20 02:25:35 +0100

    Remove unnecessary compilation flags

--- config.mk
+++ config.mk
@@ -7,7 +7,7 @@ CCMPI=
 #INCLUDES=-I$(critter_dir)/include
 INCLUDES=
 #DEFS=-DMKL -DCRITTER -DALGORITHMIC_SYMBOLS
-DEFS=-DMKL -DALGORITHMIC_SYMBOLS
+DEFS=
 #CFLAGS=-g -Wall -O3 -std=c++14 -mkl=parallel -xMIC-AVX512 ${DEFS} ${INCLUDES}
 CFLAGS=${DEFS} ${INCLUDES} -O3 -std=c++14 -fpermissive
 #LIB_PATH=-L$(critter_dir)/lib
